5e5608de79534e628cc1e3222a6f8433c8117b3c
pxlkeshi <101736130+pxlkeshi@users.noreply.github.com>
2025-04-21 12:32:40 +0400

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,8 @@
+const kelvin=0; //const stays constant
+const celsius=kelvin-273; //celsius is 273 less than kelvin
+let fahrenheit=celsius*(9/5)+32;//farhenheit to celsius equation
+fahrenheit=Math.floor(fahrenheit);
+console.log('the temperature is'+' '+fahrenheit+' '+'degress'+' '+'Fahrenheit');
+let Newton=celsius*(33/100)
+Newton=Math.floor(Newton)
+console.log('the temperature is'+' '+Newton+' '+'degress'+' '+'Newton');
